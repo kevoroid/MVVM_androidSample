a1de423ab926c6a9517fe5b99f1e8ff5f1b88e2c
kevoroid <kevoroid@gmail.com>
2019-10-27 00:07:05 +0700

    adding some ui control methods to main activity

--- app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/MainActivity.java
+++ app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/MainActivity.java
@@ -1,12 +1,18 @@
 package com.kevoroid.foodshop.ui.mainscreen;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.view.View;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.kevoroid.foodshop.R;
+import com.kevoroid.foodshop.apis.Resource;
+import com.kevoroid.foodshop.apis.Status;
 import com.kevoroid.foodshop.models.ProductList;
 import com.kevoroid.foodshop.models.viewmodels.ProductListViewModel;
 import com.kevoroid.foodshop.ui.BaseActivity;
+import com.kevoroid.foodshop.utils.NetworkHandler;
+import com.kevoroid.foodshop.utils.PromptHandler;
 
 import java.util.List;
 
@@ -14,19 +20,58 @@ public class MainActivity extends BaseActivity {
 
 	private ProductListViewModel productListViewModel;
 
+	// I know its deprecated and better to use Progressbar but for sake of simplicity!
+	private ProgressDialog progressDialog;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setMessage(getResources().getString(R.string.label_please_wait));
+		progressDialog.setIndeterminate(true);
+		progressDialog.setCancelable(false);
+
 		productListViewModel = ViewModelProviders.of(this).get(ProductListViewModel.class);
-		productListViewModel.getProductList().observe(this, new Observer<List<ProductList>>() {
-			@Override
-			public void onChanged(List<ProductList> productLists) {
-				System.out.println("MainActivity.onChanged :: " + productLists.get(0).getName());
-				System.out.println("MainActivity.onChanged :: " + productLists.get(0).getProducts().size());
-				System.out.println("MainActivity.onChanged :: " + productLists.get(1).getName());
-			}
-		});
+//		if (NetworkHandler.internetAvailable(this)) {
+			productListViewModel.getProductList().observe(this, getStuff());
+//		} else {
+//			showErr();
+//		}
+	}
+
+	private Observer<Resource<List<ProductList>>> getStuff() {
+		return productLists -> {
+//			if (productLists.status.equals(Status.ERROR)) {
+//				System.out.println("MainActivity.getStuff >>>>>> " + productLists);
+//				System.out.println("MainActivity.getStuff >>>>>> " + productLists.data);
+//				System.out.println("MainActivity.getStuff >>>>>> " + productLists.message);
+//				System.out.println("MainActivity.getStuff >>>>>> " + productLists.status);
+//			} else {
+//				if (productLists.data != null) {
+//					System.out.println("MainActivity.onChanged :: " + productLists.data.get(0).getName());
+//					System.out.println("MainActivity.onChanged :: " + productLists.data.get(0).getProducts().size());
+//					System.out.println("MainActivity.onChanged :: " + productLists.data.get(1).getName());
+//				}
+//			}
+		};
+	}
+
+	public void showLoading() {
+		progressDialog.show();
+	}
+
+	public void hideLoading() {
+		progressDialog.dismiss();
+	}
+
+	public void showErr() {
+		hideLoading();
+		PromptHandler.showErrSnackBar(getMainLayout(), this);
+	}
+
+	public View getMainLayout() {
+		return findViewById(android.R.id.content);
 	}
 }
