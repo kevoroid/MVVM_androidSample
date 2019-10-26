bbd14a17c9f2a17a007a6211b9586ac6bde13a10
kevoroid <kevoroid@gmail.com>
2019-10-26 17:28:03 +0700

    adding base activity and application class

--- app/src/main/java/com/kevoroid/foodshop/MainActivity.java
+++ app/src/main/java/com/kevoroid/foodshop/MainActivity.java
@@ -1,9 +1,8 @@
 package com.kevoroid.foodshop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/kevoroid/foodshop/YummyApplication.java
@@ -0,0 +1,11 @@
+package com.kevoroid.foodshop;
+
+import android.app.Application;
+
+public class YummyApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
