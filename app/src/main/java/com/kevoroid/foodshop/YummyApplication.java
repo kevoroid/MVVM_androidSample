b10cb856fb980a897f434c5f8d0f457b16fd713c
kevoroid <kevoroid@gmail.com>
2019-10-26 17:33:34 +0700

    update to base and app class imports based on androidx

--- app/src/main/java/com/kevoroid/foodshop/YummyApplication.java
+++ app/src/main/java/com/kevoroid/foodshop/YummyApplication.java
@@ -1,11 +1,20 @@
 package com.kevoroid.foodshop;
 
 import android.app.Application;
+import android.content.Context;
 
 public class YummyApplication extends Application {
+
+	private Context mContext;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		mContext = getApplicationContext();
+	}
+
+	public Context getContext() {
+		return mContext;
 	}
 }
