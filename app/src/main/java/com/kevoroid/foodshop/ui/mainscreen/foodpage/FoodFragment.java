53b76f50072fa9216bd98797fc9afc73c6c5d907
kevoroid <kevoroid@gmail.com>
2019-10-29 10:53:33 +0700

    updating one of the fragments to grid

--- app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/foodpage/FoodFragment.java
+++ app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/foodpage/FoodFragment.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.kevoroid.foodshop.R;
 import com.kevoroid.foodshop.models.Product;
@@ -53,7 +53,7 @@ public class FoodFragment extends BaseFragment implements RecyclerViewCallback {
 
 		foodAdapter = new FoodAdapter(this, arrayList);
 		recyclerView = view.findViewById(R.id.food_recyclerview);
-		recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+		recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 		recyclerView.setAdapter(foodAdapter);
 	}
 
