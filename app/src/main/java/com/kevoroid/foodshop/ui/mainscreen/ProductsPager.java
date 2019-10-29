be23a0708f11b1cbff3e2fd753891b41e3cfb883
kevoroid <kevoroid@gmail.com>
2019-10-29 16:16:16 +0700

    null data check for pageadapter

--- app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/ProductsPager.java
+++ app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/ProductsPager.java
@@ -1,5 +1,6 @@
 package com.kevoroid.foodshop.ui.mainscreen;
 
+import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -9,9 +10,12 @@ import com.kevoroid.foodshop.models.ProductList;
 import com.kevoroid.foodshop.ui.mainscreen.drinkspage.DrinksFragment;
 import com.kevoroid.foodshop.ui.mainscreen.foodpage.FoodFragment;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProductsPager extends FragmentPagerAdapter {
+
+	private static final String TAG = "ProductsPager";
 
 	private final int TABS_COUNT = 2;
 	private String[] tabTitles = {"Food", "Drinks"};
@@ -20,6 +24,7 @@ public class ProductsPager extends FragmentPagerAdapter {
 
 	public ProductsPager(@NonNull FragmentManager fm, int behavior, List<ProductList> data) {
 		super(fm, behavior);
+		Log.d(TAG, "ProductsPager: created!");
 		this.data = data;
 	}
 
@@ -27,9 +32,17 @@ public class ProductsPager extends FragmentPagerAdapter {
 	@Override
 	public Fragment getItem(int position) {
 		if (position == 0) {
-			return FoodFragment.newInstance(data.get(0).getProducts());
+			if (data != null) {
+				return FoodFragment.newInstance(data.get(0).getProducts());
+			} else {
+				return FoodFragment.newInstance(new ArrayList<>());
+			}
 		} else {
-			return DrinksFragment.newInstance(data.get(1).getProducts());
+			if (data != null) {
+				return DrinksFragment.newInstance(data.get(1).getProducts());
+			} else {
+				return DrinksFragment.newInstance(new ArrayList<>());
+			}
 		}
 	}
 
