09323cce894a0b7ac5c741d59a49390a8890a276
kevoroid <kevoroid@gmail.com>
2019-10-30 14:56:42 +0700

    adding sample addvalue method to the viewmodel

--- app/src/main/java/com/kevoroid/foodshop/models/viewmodels/ProductListViewModel.java
+++ app/src/main/java/com/kevoroid/foodshop/models/viewmodels/ProductListViewModel.java
@@ -3,6 +3,7 @@ package com.kevoroid.foodshop.models.viewmodels;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import com.kevoroid.foodshop.models.Product;
 import com.kevoroid.foodshop.models.ProductList;
 import com.kevoroid.foodshop.models.repos.MasterRepo;
 
@@ -12,10 +13,11 @@ public class ProductListViewModel extends ViewModel {
 
 	private MasterRepo masterRepo;
 
+	//private LiveData<Resource<List<ProductList>>> productList; // try using Resource<T>
 	private MutableLiveData<List<ProductList>> productList;
 
 	public void init() {
-		if (productList != null ) {
+		if (productList != null) {
 			return;
 		}
 
@@ -23,7 +25,20 @@ public class ProductListViewModel extends ViewModel {
 		productList = masterRepo.getProductList();
 	}
 
+	//public LiveData<Resource<List<ProductList>>> getProductList() { // try using Resource<T>
 	public LiveData<List<ProductList>> getProductList() {
 		return productList;
+	}
+
+	/**
+	 * @param productListIndex -> 0 for Food, 1 for Drinks
+	 * @param product
+	 */
+	public void addNewProduct(int productListIndex, Product product) {
+		List<ProductList> productsInStock = productList.getValue();
+		if (productsInStock != null) {
+			productsInStock.get(productListIndex).getProducts().add(product);
+		}
+		productList.postValue(productsInStock);
 	}
 }
