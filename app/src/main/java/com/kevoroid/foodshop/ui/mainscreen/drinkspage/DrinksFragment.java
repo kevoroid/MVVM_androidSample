2655d8edee65e68cbae2b1cbc7024b3c0845d19f
kevoroid <kevoroid@gmail.com>
2019-10-30 15:05:19 +0700

    adding viewmodel to the fragments and addnew value

--- app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/drinkspage/DrinksFragment.java
+++ app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/drinkspage/DrinksFragment.java
@@ -2,20 +2,27 @@ package com.kevoroid.foodshop.ui.mainscreen.drinkspage;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.kevoroid.foodshop.R;
 import com.kevoroid.foodshop.models.Product;
+import com.kevoroid.foodshop.models.ProductList;
+import com.kevoroid.foodshop.models.viewmodels.ProductListViewModel;
 import com.kevoroid.foodshop.ui.BaseFragment;
 import com.kevoroid.foodshop.ui.mainscreen.RecyclerViewCallback;
 import com.kevoroid.foodshop.utils.BottomSheetHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class DrinksFragment extends BaseFragment implements RecyclerViewCallback {
 
@@ -23,6 +30,9 @@ public class DrinksFragment extends BaseFragment implements RecyclerViewCallback
 
 	private RecyclerView recyclerView;
 	private DrinksAdapter drinksAdapter;
+	private ArrayList<Product> arrayList;
+
+	private ProductListViewModel productListViewModel;
 
 	public static DrinksFragment newInstance(List<Product> drinkList) {
 		Bundle args = new Bundle();
@@ -35,6 +45,7 @@ public class DrinksFragment extends BaseFragment implements RecyclerViewCallback
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 	}
 
 	@Override
@@ -46,7 +57,17 @@ public class DrinksFragment extends BaseFragment implements RecyclerViewCallback
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-		ArrayList<Product> arrayList  = null;
+		productListViewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(ProductListViewModel.class);
+		productListViewModel.getProductList().observe(getActivity(), productLists -> {
+			if (arrayList != null && productListViewModel.getProductList().getValue() != null) {
+				arrayList.clear();
+				arrayList.addAll(productListViewModel.getProductList().getValue().get(1).getProducts());
+				drinksAdapter.notifyDataSetChanged();
+				recyclerView.smoothScrollToPosition(productListViewModel.getProductList().getValue().get(1).getProducts().size() - 1);
+//					hideLoading();
+			}
+		});
+
 		if (getArguments() != null) {
 			arrayList = getArguments().getParcelableArrayList(DRINKS_BUNDLE);
 		}
@@ -55,6 +76,22 @@ public class DrinksFragment extends BaseFragment implements RecyclerViewCallback
 		recyclerView = view.findViewById(R.id.drinks_recyclerview);
 		recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 		recyclerView.setAdapter(drinksAdapter);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.main_menu_add_item:
+				Toast.makeText(getContext(), getResources().getString(R.string.msg_new_product_added_drink), Toast.LENGTH_SHORT).show();
+				addNewDrinks();
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void addNewDrinks() {
+//		showLoading();
+		productListViewModel.addNewProduct(1, new Product("Wine"));
 	}
 
 	@Override
--- app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/foodpage/FoodFragment.java
+++ app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/foodpage/FoodFragment.java
@@ -2,20 +2,27 @@ package com.kevoroid.foodshop.ui.mainscreen.foodpage;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.kevoroid.foodshop.R;
 import com.kevoroid.foodshop.models.Product;
+import com.kevoroid.foodshop.models.ProductList;
+import com.kevoroid.foodshop.models.viewmodels.ProductListViewModel;
 import com.kevoroid.foodshop.ui.BaseFragment;
 import com.kevoroid.foodshop.ui.mainscreen.RecyclerViewCallback;
 import com.kevoroid.foodshop.utils.BottomSheetHelper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class FoodFragment extends BaseFragment implements RecyclerViewCallback {
 
@@ -23,6 +30,9 @@ public class FoodFragment extends BaseFragment implements RecyclerViewCallback {
 
 	private RecyclerView recyclerView;
 	private FoodAdapter foodAdapter;
+	private List<Product> arrayList;
+
+	private ProductListViewModel productListViewModel;
 
 	public static FoodFragment newInstance(List<Product> foodList) {
 		Bundle args = new Bundle();
@@ -35,6 +45,7 @@ public class FoodFragment extends BaseFragment implements RecyclerViewCallback {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
 	}
 
 	@Override
@@ -46,7 +57,17 @@ public class FoodFragment extends BaseFragment implements RecyclerViewCallback {
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-		ArrayList<Product> arrayList  = null;
+		productListViewModel = ViewModelProviders.of(Objects.requireNonNull(getActivity())).get(ProductListViewModel.class);
+		productListViewModel.getProductList().observe(getActivity(), productLists -> {
+			if (arrayList != null && productListViewModel.getProductList().getValue() != null) {
+				arrayList.clear();
+				arrayList.addAll(productListViewModel.getProductList().getValue().get(0).getProducts());
+				foodAdapter.notifyDataSetChanged();
+				recyclerView.smoothScrollToPosition(productListViewModel.getProductList().getValue().get(0).getProducts().size() - 1);
+//					hideLoading();
+			}
+		});
+
 		if (getArguments() != null) {
 			arrayList = getArguments().getParcelableArrayList(FOOD_BUNDLE);
 		}
@@ -55,6 +76,22 @@ public class FoodFragment extends BaseFragment implements RecyclerViewCallback {
 		recyclerView = view.findViewById(R.id.food_recyclerview);
 		recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
 		recyclerView.setAdapter(foodAdapter);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+		switch (item.getItemId()) {
+			case R.id.main_menu_add_item:
+				Toast.makeText(getContext(), getResources().getString(R.string.msg_new_product_added_food), Toast.LENGTH_SHORT).show();
+				addNewFood();
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+	private void addNewFood() {
+//		showLoading();
+		productListViewModel.addNewProduct(0, new Product("Pizza"));
 	}
 
 	@Override
