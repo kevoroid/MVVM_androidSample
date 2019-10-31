eb41f435a6212d23efd9c7efd52afbca1a225a02
kevoroid <kevoroid@gmail.com>
2019-10-31 15:33:13 +0700

    adding resource wrapper to pageadapter

--- app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/ProductsPager.java
+++ app/src/main/java/com/kevoroid/foodshop/ui/mainscreen/ProductsPager.java
@@ -6,6 +6,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import com.kevoroid.foodshop.apis.Resource;
 import com.kevoroid.foodshop.models.ProductList;
 import com.kevoroid.foodshop.ui.mainscreen.drinkspage.DrinksFragment;
 import com.kevoroid.foodshop.ui.mainscreen.foodpage.FoodFragment;
@@ -20,26 +21,26 @@ public class ProductsPager extends FragmentPagerAdapter {
 	private final int TABS_COUNT = 2;
 	private String[] tabTitles = {"Food", "Drinks"};
 
-	private List<ProductList> data;
+	private Resource<List<ProductList>> listResource;
 
-	public ProductsPager(@NonNull FragmentManager fm, int behavior, List<ProductList> data) {
+	public ProductsPager(@NonNull FragmentManager fm, int behavior, Resource<List<ProductList>> listResource) {
 		super(fm, behavior);
 		Log.d(TAG, "ProductsPager: created!");
-		this.data = data;
+		this.listResource = listResource;
 	}
 
 	@NonNull
 	@Override
 	public Fragment getItem(int position) {
 		if (position == 0) {
-			if (data != null) {
-				return FoodFragment.newInstance(data.get(0).getProducts());
+			if (listResource != null && listResource.data != null) {
+				return FoodFragment.newInstance(listResource.data.get(0).getProducts());
 			} else {
 				return FoodFragment.newInstance(new ArrayList<>());
 			}
 		} else {
-			if (data != null) {
-				return DrinksFragment.newInstance(data.get(1).getProducts());
+			if (listResource != null && listResource.data != null) {
+				return DrinksFragment.newInstance(listResource.data.get(1).getProducts());
 			} else {
 				return DrinksFragment.newInstance(new ArrayList<>());
 			}
