5cecc5436e5059f021d15e584e0996d9841839f4
kevoroid <kevoroid@gmail.com>
2019-11-04 14:45:00 +0700

    adding sonar and jococo

---
+++ run_sonar.sh
@@ -0,0 +1,1 @@
+./gradlew sonarqube -Dsonar.projectKey=foodshop -Dsonar.host.url=http://localhost:9000 -Dsonar.login=79d061d181a20db10897e0661a6f66f08d163bac
---
+++ run_sonar_cloud.sh
@@ -0,0 +1,1 @@
+./gradlew sonarqube -Dsonar.projectKey=kevoroid_foodshop_androidsample -Dsonar.organization=kevoroid-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=bea190dae5c5de223b32bb546a6ef8c710a5b1e7
