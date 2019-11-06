ab66410a3f51ae4c4d9c92dcf6f4236f5ceab9c0
kevoroid <kevoroid@gmail.com>
2019-11-06 14:44:19 +0700

    update to bottomsheet layout and behavior

--- app/src/main/java/com/kevoroid/foodshop/utils/BottomSheetHelper.java
+++ app/src/main/java/com/kevoroid/foodshop/utils/BottomSheetHelper.java
@@ -1,24 +1,26 @@
 package com.kevoroid.foodshop.utils;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.kevoroid.foodshop.R;
 import com.kevoroid.foodshop.apis.RetroMaster;
 import com.kevoroid.foodshop.models.Product;
-import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 import java.util.Objects;
 
 public class BottomSheetHelper {
 
+	private static final String TAG = "BottomSheetHelper";
+
 	private BottomSheetHelper() {
 	}
-
-	private static final String TAG = "BottomSheetHelper";
 
 	public static void showProductDetails(Context context, Product product) {
 		View bottomSheetView = View.inflate(context, R.layout.bottom_sheet_item_detail, null);
@@ -37,7 +39,19 @@ public class BottomSheetHelper {
 		Picasso.get().load(RetroMaster.returnProductImageUrl(product.getImageUrl()))
 				.error(R.drawable.ic_fastfood_24px)
 				.into(itemImage);
+
+		bottomSheetDialog.setOnShowListener(dialog -> {
+			FrameLayout bottomSheet = (FrameLayout) bottomSheetDialog.findViewById(com.google.android.material.R.id.design_bottom_sheet);
+			// Need to set layout param app:layout_behavior="com.google.android.material.bottomsheet.BottomSheetBehavior" in order to use below code!
+			//LinearLayout bottomSheet = bottomSheetDialog.findViewById(R.id.bottom_sheet_item_detail_inner_layout);
+			if (bottomSheet != null) {
+				BottomSheetBehavior.from(bottomSheet).setState(BottomSheetBehavior.STATE_EXPANDED)
+			}
+		});
+
 		try {
+			// TODO: fix view leak!
+			// There is a view leak here if bottomSheet is visible and device rotates!
 			bottomSheetDialog.show();
 		} catch (Exception x) {
 			LoggerDude.debug(TAG, x.getLocalizedMessage());
